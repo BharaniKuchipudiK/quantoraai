499b9d6114dc3f1c3db53bcf8a93324add4a9d7e
Bharani-KBK <bharanik.h@gmail.com>
2026-08-18 17:59:17 +0800

    fix(studio): load prompt toolbar cleanup styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './styles/density.css'
+import './styles/studio-toolbar-cleanup.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
