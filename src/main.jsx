dccfe9fc6807f2b66b17da98e9068a46c075fbfb
Bharani-KBK <bharanik.h@gmail.com>
2026-08-18 21:35:18 +0800

    Load living PCL motion layer

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './styles/density.css'
 import './styles/studio-toolbar-cleanup.css'
+import './styles/pcl-motion.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
