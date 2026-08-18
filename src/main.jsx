69537ea694116b12f9eb42eda330adbbed99382b
Bharani-KBK <bharanik.h@gmail.com>
2026-08-19 07:21:33 +0800

    Load tightened landing conclusion experiment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/density.css'
 import './styles/studio-toolbar-cleanup.css'
 import './styles/pcl-motion.css'
 import './styles/future-capabilities.css'
-import './styles/future-capabilities-sync.css'
+import './styles/landing-conclusion-pass.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
