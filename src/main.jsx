28a6c8c9ea8123359ee9f1954e8007d3d82ee578
Bharani-KBK <bharanik.h@gmail.com>
2026-08-19 14:54:32 +0800

    Install specialist workspace experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import FeedbackWidget from './components/FeedbackWidget.jsx'
+import { installSpecialistExperience } from './lib/specialist-experience.js'
 import './index.css'
 import './styles/density.css'
 import './styles/studio-toolbar-cleanup.css'
 import './styles/conversation-quality.css'
+
+installSpecialistExperience()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
