2b0b2e9bcec92b733fa082c7aa0f473b8ccab213
Bharani-KBK <bharanik.h@gmail.com>
2026-08-19 10:48:17 +0800

    Mount global feedback widget for signed-in users

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import FeedbackWidget from './components/FeedbackWidget.jsx'
 import './index.css'
 import './styles/density.css'
 import './styles/studio-toolbar-cleanup.css'
@@ -8,5 +9,6 @@ import './styles/studio-toolbar-cleanup.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <FeedbackWidget />
   </React.StrictMode>,
 )
