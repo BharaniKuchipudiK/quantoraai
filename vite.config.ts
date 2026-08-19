9555fb75296195e1f56ff88a6ff8e49601654bfa
Bharani-KBK <bharanik.h@gmail.com>
2026-08-20 07:55:20 +0800

    Restore standard Google OAuth implementation in Preview

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-        '@react-oauth/google': path.resolve(__dirname, 'src/lib/google-oauth-adapter.jsx'),
       },
     },
     server: {
