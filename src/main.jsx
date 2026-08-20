478d999c87055e16413e3bdc7809537d4b24c83a
Bharani-KBK <bharanik.h@gmail.com>
2026-08-20 18:00:35 +0800

    Install Studio response presentation helpers

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import FeedbackWidget from './components/FeedbackWidget.jsx'
 import { installSpecialistExperience } from './lib/specialist-experience.js'
 import { installAgenticWorkspaceUiPolicy } from './lib/agentic-workspace-ui-policy.js'
+import { installStudioResponsePresentation } from './lib/studio-response-presentation.js'
 import { installPromptClipboardImagePaste } from './lib/clipboard-image-paste.js'
 import './index.css'
 import './styles/density.css'
@@ -12,6 +13,7 @@ import './styles/conversation-quality.css'
 
 installSpecialistExperience()
 installAgenticWorkspaceUiPolicy()
+installStudioResponsePresentation()
 installPromptClipboardImagePaste()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
