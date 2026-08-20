bea8087f37b382c41e7f5f1cc83e54e23e1c9db4
Bharani-KBK <bharanik.h@gmail.com>
2026-08-20 11:54:19 +0800

    Install prompt clipboard image bridge

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import FeedbackWidget from './components/FeedbackWidget.jsx'
 import { installSpecialistExperience } from './lib/specialist-experience.js'
+import { installPromptClipboardImagePaste } from './lib/clipboard-image-paste.js'
 import './index.css'
 import './styles/density.css'
 import './styles/studio-toolbar-cleanup.css'
 import './styles/conversation-quality.css'
 
 installSpecialistExperience()
+installPromptClipboardImagePaste()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
